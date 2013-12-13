57dc2a3811a2760a48feb1cc37a03e99a6938ecc
Kris Ferrario <kferrario@gmail.com>
2013-12-13 08:57:36 -0500

    Initial setup of morning prayer service

---
+++ views/v_services_us_morning.php
@@ -0,0 +1,86 @@
+<div id=menu>
+
+<h1>Morning Prayer</h1>
+    
+    <h4>Opening Sentance</h4>
+    
+    <p>
+        <em>Officiant:</em>
+        <?php
+                $sentances = array("Watch , for you know not when the master of the house 
+                                        will come, in the evening, or at midnight, or at cockcrow, or 
+                                        in the morning; lest he come suddenly and find you asleep. 
+                                        Mark 13:35, 36",
+        
+                                        "In the wilderness prepare the way of the Lord, make straight 
+                                        in the desert a highway for our God.    Isaiah 40:3", 
+                                        
+                                        "The glory of the Lord shall be revealed, and all flesh shall see 
+                                        it together.    Isaiah 40:5 ", 
+                                        
+                                        "Behold, I bring you good news of a great joy, which will come 
+                                        to all the people; for unto you is born this day in the city of 
+                                        David, a Savior, who is Christ the Lord.    Luke 2:10, 11", 
+                                        
+                                        "Behold, the dwelling of God is with mankind. He will dwell 
+                                        with them, and they shall be his people, and God himself will 
+                                        be with them, and be their God.    Revelation 21:3");
+                $rand_keys = array_rand($sentances);
+        echo $sentances[$rand_keys] . "\n";
+        ?>
+    </p>
+    
+    
+    <h4>The Invitatory and Psalter</h4>
+
+    <h5>All stand</h5></br>
+    
+        <p><em>Officiant:</em>    Lord, open our lips.</p> 
+        <p><em>People:</em>      And our mouth shall proclaim your praise.</p> 
+    
+        <p><em>Officiant and People:</em> Glory to the Father, and to the Son, and to the Holy Spirit: as 
+        it was in the beginning, is now, and will be for ever. <em>Amen. Alleluia.</em></p>
+        
+    
+    <h4>The Psalm or Psalms Appointed</h4>
+    
+    <p>Glory to the Father, and to the Son, and to the Holy Spirit: *</br> 
+    as it was in the beginning, is now, and will be for ever. Amen.</p>
+    
+    <h4>The Lessons</h4>
+    
+    <script language="JavaScript" src="http://feed2js.org//feed2js.php?src=http%3A%2F%2Ffeeds.feedburner.com%2FESV-bcp&chan=y&num=1&utf=y&html=a"  charset="UTF-8" type="text/javascript"></script>
+    
+    <noscript>
+    <a href="http://feed2js.org//feed2js.php?src=http%3A%2F%2Ffeeds.feedburner.com%2FESV-bcp&chan=y&num=1&utf=y&html=y">View RSS feed</a>
+    </noscript>
+    
+        <p>The Word of the Lord.</p> 
+        <p><strong>Thanks be to God.</strong></p>
+        
+    <h4>A Canticle</h4>
+    
+    <h4>The Apostles' Creed</h4>
+
+    <p><em>Officiant and People together, all standing</em></p>
+    
+    <p>I believe in God, the Father almighty,</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;creator of heaven and earth;</br> 
+    I believe in Jesus Christ, his only Son, our Lord.</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;He was conceived by the power of the Holy Spirit</br> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and born of the Virgin Mary.</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;He suffered under Pontius Pilate,</br> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;was crucified, died, and was buried.</br>  
+            &nbsp;&nbsp;&nbsp;&nbsp;He descended to the dead.</br>  
+            &nbsp;&nbsp;&nbsp;&nbsp;On the third day he rose again.</br>  
+            &nbsp;&nbsp;&nbsp;&nbsp;He ascended into heaven,</br> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and is seated at the right hand of the Father.</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;He will come again to judge the living and the dead.</br> 
+    I believe in the Holy Spirit,</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;the holy catholic Church,</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;the communion of saints,</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;the forgiveness of sins</br>
+            &nbsp;&nbsp;&nbsp;&nbsp;the resurrection of the body,</br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;and the life everlasting. Amen.</p>
+    
+    <h4></h4>
