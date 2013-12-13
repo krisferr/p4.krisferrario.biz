495527aa4d35c4b0c2dee81b7286f7a9b9694fea
Kris Ferrario <kferrario@gmail.com>
2013-12-13 13:59:52 -0500

    New format of date

--- js/prayer.js
+++ js/prayer.js
@@ -12,7 +12,7 @@ function calculate_date() {
   month       = currentTime.getMonth() + 1; //returns values 0-11 so +1 is required
   day         = currentTime.getDate();
   year        = currentTime.getFullYear();
-  calcdate    = month + "/" + day + "/" + year;
+  calcdate    = year + "-" + month + "-" + day;
   return calcdate;
 }
 
@@ -21,3 +21,7 @@ function prepopulate_date() {
 }
 
 window.onload = prepopulate_date;
+
+function passing_date(){
+     window.location.href = "c_base.php?date=" + document.getElementById("datepicker").setAttribute('value', calculate_date());
+}
