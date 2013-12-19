1b69f9003cd56dc31b21fe79ed5b122addc17fb6
Kris Ferrario <kferrario@gmail.com>
2013-12-19 14:33:07 -0500

    First commit of a page with a video

---
+++ views/v_services_whyprayer.php
@@ -0,0 +1,24 @@
+<?php if($user): ?>
+	<h3 id=greeting>Hello, <a href="/users/profile"><?=$user->first_name;?></a>!</h3>
+
+
+<?php else: ?>
+	<h3 id=greeting>
+		Hello, Guest!</br>
+		<a href="/users/signup">Please registar</a>
+		or <a href="/users/login">Log In</a>
+	</h3>
+	
+<?php endif; ?>
+
+
+
+<div id=video>
+			<iframe width="640" height="480" src="//www.youtube.com/embed/T6PMAU4FjXU" frameborder="0" allowfullscreen></iframe>
+						</br>
+						<form style="display: inline" action="/" method="get">
+							<button>Back</button></br>
+						</form></br>
+</div>
+
+
