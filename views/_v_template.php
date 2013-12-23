763847088028f6bcef191c39ce8f8ebd8c7cb25b
Kris Ferrario <kferrario@gmail.com>
2013-12-22 20:56:51 -0500

    changed file paths

--- views/_v_template.php
+++ views/_v_template.php
@@ -5,28 +5,27 @@
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	
-	<link rel="icon" type="image/vnd.microsoft.icon" href="../images/icon.ico" />
+	<link rel="icon" type="image/vnd.microsoft.icon" href="../../images/icon.ico" />
 					
 	<!-- Controller Specific JS/CSS -->
-	<link rel="stylesheet" href="../css/main.css" type="text/css">
+	<link rel="stylesheet" href="../../css/main.css" type="text/css">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine:bold,bolditalic|Inconsolata:italic|Droid+Sans">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Old+Standard%20TT:400italic,400,700">
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-	<script src="../js/prayer.js"></script>
+	<script src="../../js/prayer.js"></script>
 	
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
 <body>	
-
+	
 	<?php if(isset($content)) echo $content; ?>
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
-	
+	<?php if(isset($client_files_body)) echo $client_files_body; ?>	
 	
 </body>
 
